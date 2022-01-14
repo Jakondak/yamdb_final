d77cfbe90386b67381d468a2d9d878ed32aaef43
Jakondak <42607365+Jakondak@users.noreply.github.com>
2022-01-14 21:35:12 +0300

    Update settings.py

--- api_yamdb/api_yamdb/settings.py
+++ api_yamdb/api_yamdb/settings.py
@@ -56,14 +56,25 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'api_yamdb.wsgi.application'
 
+DATABASES = { 
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
+    'default': { 
 
+        'ENGINE': os.getenv('DB_ENGINE', default='django.db.backends.postgresql'), 
+
+        'NAME': os.getenv('DB_NAME', default='postgres'), 
+
+        'USER': os.getenv('POSTGRES_USER', default='postgres'), 
+
+        'PASSWORD': os.getenv('POSTGRES_PASSWORD', default='postgres'), 
+
+        'HOST': os.getenv('DB_HOST', default='db'), 
+
+        'PORT': os.getenv('DB_PORT', default='5432') 
+
+    } 
+
+} 
 
 AUTH_PASSWORD_VALIDATORS = [
     {
